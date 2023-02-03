510f7106c6fe7945b14b8110726f27e75bed650d
mathminx <122234007+mathminx@users.noreply.github.com>
2023-02-03 16:34:18 -0400

    updates to js

--- Develop/script.js
+++ Develop/script.js
@@ -1,38 +1,98 @@
+var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var smallLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var special = ["!", "#", "$", "%", "&", "(", ")", "*", "+", "-", "/", ":", ";", "<", "=", ">", "?", "@", "[", "^"];
 
-//button to generate password
+var selectedCharacters = capitalLetters.concat(smallLetters, digits, special); 
+var minLength = 8;
+var maxLength = 120;
+var newPassword = "";
+var chosenLength ="";
+var yesOrNo = ['y', 'Y', 'n', 'N']
 
-//password criteria: 
-  //  - length between 8 and 128 characters
-      //validate length
-  //  - character types:
-      //    - lowercase
-      //    - uppercase
-      //    - numeric
-      //    - special
-          // list of special characters as string: " !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+//Ask user for the desired password length
+function chooseLength () {
+chosenLength ="";
+var getLength = window.prompt("Enter a length between 8 and 120 characters for your password.");
+if (getLength < minLength) {
+    alert("The number you input is less than 8");
+    chooseLength();
+} else if (getLength > maxLength) {
+alert("The number you input is more than 120");
+chooseLength();
+}  else chosenLength = getLength;
+}
 
-//when all prompts are answered:
-    //validate that at least one character type has been chosen
+//Ask user if they want to include uppercase letters 
+function chooseUppercase() {
+var includeUppercase = window.prompt("Include uppercase letters?. (Y/N)");
+if (!yesOrNo.includes(includeUppercase)) {
+  alert("Please input Y or N");
+  chooseUppercase()
+}
+}
 
-//generate password that matches the selected criteria
+//Ask user if they want to include lowercase letters 
+function chooseLowercase() {
+  var includeLowercase = window.prompt("Include lowercase letters?. (Y/N)");
+  if (!yesOrNo.includes(includeLowercase)) {
+    alert("Please input Y or N");
+    chooseLowercase()
+  }
+  }
+  
+//Ask user if they want to include numbers 
+function chooseNumbers() {
+  var includeNumbers = window.prompt("Include numbers?. (Y/N)");
+  if (!yesOrNo.includes(includeNumbers)) {
+    alert("Please input Y or N");
+    chooseLowercase()
+  }
+  }
+  
+//Ask user if they want to include special characters 
+function chooseSpecialChars() {
+  var includeSpecialChars = window.prompt("Include special characters?. (Y/N)");
+  if (!yesOrNo.includes(includeSpecialChars)) {
+    alert("Please input Y or N");
+    chooseSpecialChars()
+  }
+  }
+  
+//Lets user choose which types of characters to include in the password
+function getParameters () {
+  chooseLength();
+  chooseUppercase();
+  chooseLowercase ();
+  chooseNumbers();
+  chooseSpecialChars();
+}
 
-//display the password in an alert or write it to the page
+//Generate a random password
+function generatePassword () {
+  newPassword="";
+  for (var i = 0; i < chosenLength; i++) {
+    var index = Math.floor(Math.random()*selectedCharacters.length);
+    var randomChoice = selectedCharacters[index];
+    newPassword = newPassword + randomChoice;
+  }
+}
 
-
-
-
+// Write the password to the #password input
+function writePassword() {
+  var passwordText = document.querySelector("#password");
+  passwordText.value = newPassword;
+}
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", passwordMaker);
+
+//Function to generate and display a random password 
+function passwordMaker() {
+  getParameters();
+  generatePassword();
+  writePassword();
+} 
